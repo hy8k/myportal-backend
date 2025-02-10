b4d9534c58e6720b43b74c833799677039f6cec5
hyyk <shiraken7101@gmail.com>
2025-02-10 18:21:05 +0900

    update

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express()
 const apiRouter = require("./api")
 const port = 3000
 
+
+app.use(cors({
+    origin: 'https://hyyk-myportal.vercel.app',
+    credentials: true, 
+    optionsSuccessStatus: 200
+}))
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
