4f7ce2a2910f42ac83114ee4946c664ed65c0e2b
hyyk <shiraken7101@gmail.com>
2025-02-10 17:12:29 +0900

    update

--- prisma/migrations/20250209155002_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "memos" (
-    "title" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "content" TEXT NOT NULL
-);
